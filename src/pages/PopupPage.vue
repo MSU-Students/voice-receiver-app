eaf748ec9621826e50154c79650049012e3577d0
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-06-18 22:10:38 +0800

    changed status text.

--- src/pages/PopupPage.vue
+++ src/pages/PopupPage.vue
@@ -16,11 +16,11 @@
               self="center middle"
               :offset="[10, 10]"
             >
-              <strong>Mute My Speaker </strong>
+              <strong>Press to Mute</strong>
               <q-icon size="20px" name="volume_off" />
             </q-tooltip>
           </q-btn>
-          <div class="row justify-center text-overline">
+          <div class="row justify-center text-overline q-pt-sm">
             {{ status }}
             <q-spinner-audio color="primary" size="2em" />
           </div>
@@ -50,7 +50,7 @@ export default {
   data() {
     return {
       isSpeakerOn: true,
-      status: "Speaking..."
+      status: "Listening..."
     };
   },
   methods: {
