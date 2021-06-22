4631e655766fe654d8cc892e76e053e9bca8825a
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-06-22 14:31:13 +0800

    added form for area profile.

--- src/pages/InstituitionInfo.vue
+++ src/pages/InstituitionInfo.vue
@@ -1,18 +1,62 @@
 <template>
   <q-page style="min-width: 440px">
-    <q-toolbar class="text-white" style="background: linear-gradient(145deg, #15503e 15%, #133154 70%);">
-        <q-btn icon="arrow_back" flat label="Back" to="/popup"/>
-        <q-space />
+    <q-toolbar
+      class="text-white"
+      style="background: linear-gradient(145deg, #15503e 15%, #133154 70%);"
+    >
+      <q-btn icon="arrow_back" flat label="Back" to="/popup" />
+      <q-space />
+    </q-toolbar>
+    <div class="q-pa-md flex flex-center" style="max-width: 400px">
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <q-input
+          filled
+          v-model="area.officeName"
+          label="College/Office Name"
+          hint="college or office"
+          lazy-rules
+          :rules="[val => (val && val.length > 0) || 'Please type something.']"
+        />
 
-      </q-toolbar>
-    <div>
-      College Profile Page
+        <q-input
+          filled
+          type="number"
+          v-model="area.codeNum"
+          label="Code #."
+          hint="sample: 1001"
+          lazy-rules
+          :rules="[
+            val => (val !== null && val !== '') || 'Please type the code #.',
+            val => (val > 0 && val < 1001) || 'Invalid code Num.'
+          ]"
+        />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+        </div>
+      </q-form>
     </div>
   </q-page>
 </template>
-
 <script>
 export default {
-  name: 'InstituitionPage'
-}
+  data() {
+    return {
+      area: {
+        officeName: null,
+        codeNum: null
+      },
+
+
+    };
+  },
+
+  methods: {
+    onSubmit() {
+
+      }
+    },
+
+  
+};
 </script>
