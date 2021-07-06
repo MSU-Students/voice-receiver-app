ede2dec1e0d26a5ddb6deb5017ce531d9e579232
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-07-06 21:53:17 +0800

    added service api for connect server.

---
+++ src/services/server-connection.service.js
@@ -0,0 +1,27 @@
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client";
+
+class ServerConnectionService {
+  async connect() {
+    this.socket = new SockJS("http://localhost:8090/gs-guide-websocket");
+    this.stompClient = Stomp.over(this.socket);
+    await this.stompClient.connect(
+      {},
+      frame => {
+        console.log(frame);
+        this.stompClient.subscribe("/topic/greetings", tick => {
+          console.log(tick);
+        });
+      },
+      error => {
+        console.log(error);
+        console.log("Cannot connect to server.");
+      }
+    );
+  }
+  
+}
+
+let serverConnectionService = new ServerConnectionService();
+
+export default serverConnectionService;
