72df401ad2689d4fb6ae67544ca73706c1c0c9fe
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-07-07 22:51:47 +0800

    added disable btn.

--- src/components/ConnectServer.vue
+++ src/components/ConnectServer.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="q-gutter-xs">
     <q-btn
+      :disable="isDisableConnect"
       align="left"
       class="btn-fixed-width"
-      color="primary"
       label="connect server"
       size="10px"
+      @click.prevent="connectServer"
     />
     <q-btn
+      :disable="isDisableDisconnect"
       align="right"
       class="btn-fixed-width"
-      color="secondary"
+      color="red-12"
       label="Disconnect"
       size="10px"
+      @click.prevent="disconnect"
     />
   </div>
 </template>
+<script>
+import serverConnectionService from "../services/server-connection.service.js";
+export default {
+  name: "ConnectServer",
+  data() {
+    return {
+      showConnectLoader: false,
+      isConnected: false,
+      isDisableConnect: false,
+      isDisableDisconnect: true
+    };
+  },
+  methods: {
+    async connectServer() {
+      this.isDisableConnect = true;
+      this.isDisableDisconnect = false;
+      await serverConnectionService.connect();
+      this.isConnected = true;
+    },
+
+    async disconnect() {
+      this.isConnected = false;
+      this.isDisableConnect = false;
+      this.isDisableDisconnect = true;
+      await serverConnectionService.disconnect();
+    }
+  }
+};
+</script>
